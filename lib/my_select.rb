e71359cc9430b29d549b84795357accce776e5c3
Lige <lige.williamson@gmail.com>
2019-02-14 02:32:01 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,8 @@ def my_select(collection)
   i = 0
   truthy_collection = []
   while i < collection.length
-    if yield(array[i]) == true
-      truthy_collection << yield(array[i])
+    if yield(collection[i]) == true
+      truthy_collection << yield(collection[i])
     end
     i += 1
   end
